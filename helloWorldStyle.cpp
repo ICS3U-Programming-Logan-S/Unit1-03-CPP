66e66fc6eab172437091f9d5a7a692c59b1a5865
Ubuntu <ubuntu@ip-172-31-91-42.ec2.internal>
2021-11-25 18:12:21 +0000

    Created helloWorldStyle.cpp with STYLE

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Logan Sweeney All rights reserved.
+// .
+// Created by: Logan
+// Date: November 25, 2021
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+}
